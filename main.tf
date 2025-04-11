d01c0e000e968ff420822a50052794a10b0ca87c
ShrutiKatkar <shrutikatkar02@gmail.com>
2025-04-11 10:37:38 +0530

    comment access key n password

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region  = "ap-south-1"
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  #access_key = "AWS_ACCESS_KEY_ID"
+  #secret_key = "AWS_SECRET_ACCESS_KEY"
 }
 
 resource "aws_instance" "example_server" {
