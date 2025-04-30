5d155759a950b0dd91b65f2ab252d215fb221431
ShrutiKatkar <shrutikatkar02@gmail.com>
2025-04-30 18:43:58 +0530

    creating security groups

--- VPC_2Tier.tf
+++ VPC_2Tier.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "test_vpc" {
   }
 }
 
-#create a public and privatee subnet in 1 AZ 
+#create a public and private subnet in 1 AZ 
 resource "aws_subnet" "test_public_subnet1" {
   vpc_id     = aws_vpc.test_vpc.id
   cidr_block = "10.0.0.0/24"
@@ -50,6 +50,7 @@ resource "aws_instance" "instance1" {
   instance_type = "t2.micro"               
   key_name      = "DemoUser1_KeyPair"
   subnet_id     = aws_subnet.test_private_subnet1.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
     Name = "Private 1a"
   }
@@ -61,8 +62,52 @@ resource "aws_instance" "instance2" {
   instance_type = "t2.micro"               
   key_name      = "DemoUser1_KeyPair"
   subnet_id     = aws_subnet.test_private_subnet2.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id,aws_security_group.allow_all.id]
   tags = {
     Name = "Private 1b"
   }
 }
+
+#Creating Security Groups
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+ 
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+ 
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+#assigning ingress rules for "allow tls" security group
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  #cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
+  #from_port         = 443
+  ip_protocol       = "-1"
+  #to_port           = 443
+}
+ 
+resource "aws_vpc_security_group_ingress_rule" "allows_RDP" {
+  security_group_id = aws_security_group.allow_tls.id  
+  #cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 3389
+  ip_protocol       = "tcp"
+  to_port           = 3389
+}
+
+# creating an ALB in Public Subnet 1
+
  
