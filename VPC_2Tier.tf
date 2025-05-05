c378029650d88d7acfcfe02f4bff49bf2e869379
ShrutiKatkar <shrutikatkar02@gmail.com>
2025-05-05 18:07:40 +0530

    attaching instances to TG

--- VPC_2Tier.tf
+++ VPC_2Tier.tf
@@ -123,7 +123,30 @@ resource "aws_lb" "ALB1" {
   #internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_tls.id]
-  subnets            = [aws_subnet.test_public_subnet1.id,aws_subnet.test_public_subnet2.id]
+  #subnets            = [aws_subnet.test_public_subnet1.id,aws_subnet.test_public_subnet2.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
 
   enable_deletion_protection = false
+}
+
+#Creating Target groups
+resource "aws_lb_target_group" "test_targetgroup" {
+  name     = "tf-test-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.test_vpc.id
+}
+
+#attaching instances to Target Group
+resource "aws_lb_target_group_attachment" "test_attachment1" {
+  target_group_arn = aws_lb_target_group.test_targetgroup.arn
+  target_id        = aws_instance.instance1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "test_attachment2" {
+  target_group_arn = aws_lb_target_group.test_targetgroup.arn
+  target_id        = aws_instance.instance2.id
+  port             = 80
 }
