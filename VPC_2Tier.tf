e13becca2d09dbea405f731ee4d9473dd8243949
ShrutiKatkar <shrutikatkar02@gmail.com>
2025-05-08 11:31:49 +0530

    name

--- VPC_2Tier.tf
+++ VPC_2Tier.tf
@@ -57,7 +57,7 @@ resource "aws_route_table" "public_route" {
 # associating route table with Public subnet 1
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.test_public_subnet1.id
-  route_table_id = aws_route_table.test.id
+  route_table_id = aws_route_table.public_route.id
 }
 
 # # creating network interface for subnet
