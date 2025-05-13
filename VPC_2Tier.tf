a718c73ad0337556be9fa84d504f9f59c92a7c3e
ShrutiKatkar <shrutikatkar02@gmail.com>
2025-05-13 10:34:56 +0530

    nat

--- VPC_2Tier.tf
+++ VPC_2Tier.tf
@@ -71,7 +71,7 @@ resource "aws_route_table" "private_route" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat1.id
+    #nat_gateway_id = aws_nat_gateway.nat1.id
   }
 }
 
